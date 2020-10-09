c3a281c9fcc31832a32fb9623d747f7bd5ddedbb
JP Dillingham <jpdillingham@gmail.com>
2020-10-08 20:23:33 -0500

    tweaks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const WebSocket = require('ws');
+const url = require('url');
 
 const port = 3000;
 const app = express();
@@ -13,7 +14,7 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 const router = express.Router();
 app.use('/api', router);
 
-const chats = [];
+const chats = ['lobby'];
 
 router.get('/greeting', (req, res) => {
   res.status(200)
@@ -35,12 +36,28 @@ router.get('/chats', (req, res) => {
 
 const webSocketServer = new WebSocket.Server({ port: 8080 });
  
-webSocketServer.on('connection', function connection(webSocket) {
-  console.log(`new client connected! ${webSocketServer.clients.length} total.`);
+webSocketServer.on('connection', (webSocket, request) => {
+  const { chat } = url.parse(request.url, true).query;
+
+  if ((chat || '').length === 0) {
+    console.log('no chat specified');
+    webSocket.terminate();
+    return;
+  }
+
+  if (!chats.includes(chat)) {
+    console.log(`unknown chat ${chat}`);
+    webSocket.terminate();
+    return;
+  }
+
+  console.log(`new client connected to chat ${chat}!`);
 
   webSocket.on('message', message => {
-    console.log('received: %s', message);
+    
+    console.log('[%s] received: %s', chat, message);
     webSocketServer.clients.forEach(client => {
+      console.log(JSON.stringify(client));
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
       }
