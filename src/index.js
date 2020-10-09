9ffd4b03d206b11ff220e8b5b0990a4555188804
Alex Triece <alex.triece@gmail.com>
2020-10-08 21:17:51 -0500

    implement channels

--- src/index.js
+++ src/index.js
@@ -14,16 +14,11 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 const router = express.Router();
 app.use('/api', router);
 
-const chats = ['lobby'];
-
-router.get('/greeting', (req, res) => {
-  res.status(200)
-    .send('Hello, World!');
-});
+const chats = [{name:"lobby", clients:[]}];
 
 router.post('/chats', (req, res) => {
   const chatId = uuidv4();
-  chats.push(chatId);
+  chats.push({name:chatId, clients:[]});
   console.log("chat created " + chatId)
   res.status(201)
     .json(chatId);
@@ -35,31 +30,49 @@ router.get('/chats', (req, res) => {
 });
 
 const webSocketServer = new WebSocket.Server({ port: 8080 });
- 
+const getChatById = (id) => {
+  const foundChat = chats.find(chat => {
+    console.log(chat); 
+    if (chat.name === id){
+      return true;
+    }
+   return false;
+  })
+  return foundChat;
+} 
 webSocketServer.on('connection', (webSocket, request) => {
-  const { chat } = url.parse(request.url, true).query;
+  const { chatId } = url.parse(request.url, true).query;
 
-  if ((chat || '').length === 0) {
+  if ((chatId || '').length === 0) {
     console.log('no chat specified');
     webSocket.terminate();
     return;
   }
 
-  if (!chats.includes(chat)) {
-    console.log(`unknown chat ${chat}`);
-    webSocket.terminate();
-    return;
-  }
-
-  console.log(`new client connected to chat ${chat}!`);
-
+  const foundChat = getChatById(chatId);
+  foundChat.clients.push(webSocket);
+  console.log(`new client connected to chat ${chatId}!`);
   webSocket.on('message', message => {
     
-    console.log('[%s] received: %s', chat, message);
+    console.log('[%s] received: %s', chatId, message);
+    const foundChat = getChatById(chatId);
+    if (foundChat === undefined) {
+      console.log(`unknown chat ${chatId}`);
+      webSocket.terminate();
+      return;
+    }
+    console.log("foundChat", foundChat)
     webSocketServer.clients.forEach(client => {
-      console.log(JSON.stringify(client));
-      if (client.readyState === WebSocket.OPEN) {
+      // console.log(client);
+      if (client.readyState !== WebSocket.OPEN) {
+        return
+      }
+      if (foundChat.clients.includes(client)) {
+        console.log("match");
         client.send(message);
+      }
+      else {
+        console.log("no match");
       }
     });
   });
